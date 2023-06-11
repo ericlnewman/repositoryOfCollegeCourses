b53c5b6f6e20322c76c9f69dbdb74098095a3d35
ericlnewman <130870127+ericlnewman@users.noreply.github.com>
2023-06-11 12:04:41 -0400

    Delete CSC 405 Graphics and Visualization/Week 5/Critiical Thinking 5 directory

--- CSC 405 Graphics and Visualization/Week 5/Critiical Thinking 5/app.js
+++
@@ -1,7 +0,0 @@
-const app = require('express')();
-app.get('/', (req, res) =>
-    res.json({ message: 'Docker work'})
-    );
-    const port = process.env.PORT || 8080;
-
-    app.listen(port, () => console.log(' listening on'))
