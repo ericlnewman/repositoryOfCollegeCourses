69026b8e775d499aa9c4b06a5f34fcbbcda5972e
ericlnewman <130870127+ericlnewman@users.noreply.github.com>
2023-06-11 11:27:14 -0400

    Delete CSC 372 directory

--- CSC 372/Week 4 Shapes/Shape.java
+++
@@ -1,4 +0,0 @@
-abstract class Shape{
-    abstract double surface_area();                                      // creates an abstract method called surface area, that is a double
-    abstract double volume();                                           // creates ab abstract method called volume, that is a double
-}
